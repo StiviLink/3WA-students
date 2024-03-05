4086e47b51811ef9bd88e58b6c7c26c0d520df08
Steve Minala <minalasteve47@gmail.com>
2024-03-05 10:27:46 +0100

    Update with process.cwd()

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
+import path from "node:path"
 import express from "express"
 import bodyParser from "body-parser"
 import avatarRoute from "./routes/avatar.route.js"
 
 const app = express()
+const parent = process.cwd()
+console.log(parent)
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
@@ -10,7 +13,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 // set the view engine to ejs
 app.set('view engine', 'ejs')
-app.set('views', 'api/views')
+app.set('views', path.join(parent, `${!parent.endsWith('api') ? 'api/' : ''}views`))
 
 const host = 'localhost'
 const port = 8000
